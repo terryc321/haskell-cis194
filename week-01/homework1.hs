45940f89e2eecb81f12f23ae171fdd21fc67f7b8
terry <terry.cadd@gmail.com>
2025-12-20 16:56:46 +0000

    done exercises 1 - 4

--- week-01/homework1.hs
+++ week-01/homework1.hs
@@ -1,19 +1,5 @@
 -- homework1.lhs
 
--- double the value of every second digit beginning from the right.
--- That is, the last digit is unchanged; the second-to-last digit is dou-
--- bled; the third-to-last digit is unchanged; and so on. For example,
--- [1,3,8,6] becomes [2,3,16,6]
--- double [] = []
--- double (x : []) = [2 * x]
--- double (y : x : []) = y : (2 * x) : []
--- double (y : x : z) = y : (2*x) : (double z)
--- reverse ... reverse xs
---doubleEveryOther [] = []
--- doubleEveryOther xs = doubleEveryOther2 (reverse xs)
-
--- give each element an index
--- findex :: (Num b, Num a) => [(a, b)]
 findex [] n acc = acc
 findex (x : ys) n acc = findex ys (n+1) ((x,n) : acc)
 
@@ -22,25 +8,68 @@ fdouble (x : ys) = let (a,b) = x
                    in let r = if odd b then a else 2* a
                           in r : (fdouble ys)
 
-
 doubler xs = fdouble (findex (reverse xs) 1 [])
--- doubler [10,20,30,40,50]
--- [10,40,30,80,50]
+doublerTest = [doubler [10,20,30,40,50] == [10,40,30,80,50]]
 
 fadder xs = sum (doubler xs)
 
--- repeatedly peel off until zero remainder?
-
+foo :: Integer -> [Integer]
 foo 0 = []
-foo n = let r = rem n 10
-            d = div n 10
-        in r : (foo d)
+foo n = if n < 0 then []
+        else let r = rem n 10
+                 d = div n 10
+             in r : (foo d)
+                
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = foo n
 
 toDigits :: Integer -> [Integer]
 toDigits n = reverse (toDigitsRev n)
+
+toDigitsTest :: [Bool]
+toDigitsTest = [ toDigits 1234 == [1,2,3,4],
+                 toDigitsRev 1234 == [4,3,2,1],
+                 toDigits 0 == [],
+                 toDigits (-17) == []
+               ]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = doubler
+
+doubleEveryOtherTest :: [Bool]
+doubleEveryOtherTest = [doubleEveryOther [1,2,3] == [1,4,3] ,
+                         doubleEveryOther [8,7,6,5] == [16,7,12,5]]
+                       
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum (map sum (map toDigits xs))
+
+sumDigitsTest :: [Bool]
+sumDigitsTest = [ sumDigits [16,7,12,5] == 22 ]
+
+validate :: Integer -> Bool
+validate n = let xs = toDigits n
+                 ys = doubleEveryOther xs              
+             in let r = rem (sumDigits ys) 10
+                in r == 0
+                   
+
+validateTest :: [Bool]
+validateTest = [ validate 4012888888881881 == True ,
+                 validate 4012888888881882 == False
+               ]
+
+
+runTest = [("toDigits" , toDigitsTest),
+           ("doubler", doublerTest ),
+           ("doubleEveryOther", doubleEveryOtherTest ),
+           ("sumDigits" , sumDigitsTest),
+           ("validate" , validateTest)]
+
+-- exercise 5 tower of hanoi
+-- exercise 6 optional extended hanoi
+
+
 
 
 
